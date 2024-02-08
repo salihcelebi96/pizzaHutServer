b67074d76a8e56a895fbd0f712a19a7b34218014
salihcelebi96 <celebisalih277@gmail.com>
2024-02-08 15:09:03 +0300

    20.commit

--- api/signup.js
+++ api/signup.js
@@ -1,4 +1,3 @@
-// index.js
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -7,21 +6,9 @@ const dotenv = require('dotenv');
 const router = express.Router(); 
 const app = express();
 
-app.use(cors({
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-}));
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', '*');
-  next();
-});
-
 dotenv.config();
 
+// MongoDB bağlantısı
 mongoose.connect(process.env.MONGODB_URI);
 const db = mongoose.connection;
 
@@ -30,14 +17,37 @@ db.once('open', function () {
   console.log('MongoDB connected successfully');
 });
 
-app.use(bodyParser.json());
+// Kullanıcı modeli tanımı
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  phoneNumber: String,
+  password: String,
+  isChecked1: Boolean,
+  isChecked2: Boolean,
+  isChecked3: Boolean
+});
 
+const UserModel = mongoose.model('User', userSchema);
+
+// Middleware
+app.use(cors({
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
+
+app.use(express.json());
+
+// Routes
 router.post('/', async (req, res) => {
   try {
     const { name, email, phoneNumber, password, isChecked1, isChecked2, isChecked3 } = req.body;
-    
-    
-    res.status(200).json({ message: 'Signup successful'/*, user: savedUser */ });
+    // Yeni kullanıcı oluştur
+    const newUser = new UserModel({ name, email, phoneNumber, password, isChecked1, isChecked2, isChecked3 });
+    // Veritabanına kaydet
+    const savedUser = await newUser.save();
+    res.status(200).json({ message: 'Signup successful', user: savedUser });
   } catch (error) {
     console.error('Error during signup:', error);
     res.status(500).json({ message: 'Internal Server Error' });
@@ -46,12 +56,9 @@ router.post('/', async (req, res) => {
 
 router.get('/', async (req, res) => {
   try {
-    // UserModel kullanımını kaldırın
-    // const users = await UserModel.find({});
-    
-    // Burada gerekli işlemleri gerçekleştirin
-
-    res.status(200).json({ /*users*/ });
+    // Tüm kullanıcıları getir
+    const users = await UserModel.find({});
+    res.status(200).json({ users });
   } catch (error) {
     console.error('Error getting users:', error);
     res.status(500).json({ message: 'Internal Server Error' });
