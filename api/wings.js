48cebb19d86c0b695206a4f6e1df1d87fa160c29
salihcelebi96 <celebisalih277@gmail.com>
2024-02-25 02:09:09 +0300

    46.commit

--- api/wings.js
+++ api/wings.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 const dotenv = require('dotenv');
-
-
 const router = express.Router(); 
 
 
