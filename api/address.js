31a43e53500af6e13f77528201216487ae1c08f4
salihcelebi96 <celebisalih277@gmail.com>
2024-03-07 00:02:39 +0300

    48.commit

--- api/address.js
+++ api/address.js
@@ -55,7 +55,7 @@ router.post("/", async (req, res) => {
            neighbourHood,
            addressDetail,
            addressName,
-           user
+           user,
             
           });   
           
