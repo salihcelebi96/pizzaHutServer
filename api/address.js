6d3b6be3b5e1e910ab4aa6c23c0b46a20ba35dfa
salihcelebi96 <celebisalih277@gmail.com>
2024-03-07 15:44:47 +0300

    55.commit

--- api/address.js
+++ api/address.js
@@ -31,7 +31,7 @@ const addressSchema = new mongoose.Schema({
     
 });
 
-const Address = mongoose.model("Address", addressSchema);
+const Address = mongoose.model("address", addressSchema);
 
 router.get("/", async (req, res) => {
     try {
